4b3cd1f423182aef3cc6229948f96467f3c077a1
develop-suda <developsuda@gmail.com>
2022-05-14 22:09:08 +0900

    構造体を外出し

--- api/select/select.go
+++ api/select/select.go
@@ -4,27 +4,8 @@ import (
 	"database/sql"
 	"github.com/develop-suda/typ_engineer_API/common"
 	"fmt"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 )
-
-type word struct {
-	Word            string `json:"word"`
-	Parts_of_speech string `json:"parts_of_speech"`
-	Description     string `json:"Description"`
-}
-
-type wordType struct {
-	Word_type string `json:"word_type"`
-}
-
-type partsOfSpeech struct {
-	Parts_of_speech string `json:"parts_of_speech"`
-}
-
-type wordTypeId struct {
-	Word_type_id string
-}
 
 type user struct {
 	Name         string
@@ -32,11 +13,11 @@ type user struct {
 	Phone_number string
 }
 
-func GetTypWords(db *sql.DB, values map[string]string) []word {
+func GetTypWords(db *sql.DB, values map[string]string) []def.Word {
 
 	// 複数件取得する場合、構造体を配列にする
-	var words []word
-	var onlyWordTypeId []wordTypeId
+	var words []def.Word
+	var onlyWordTypeId []def.WordTypeId
 
 	sql := def.GET_WORD_TYPE_ID_SQL
 	sql = fmt.Sprintf(sql, values["type"])
@@ -44,7 +25,7 @@ func GetTypWords(db *sql.DB, values map[string]string) []word {
 	result, err := db.Query(sql)
 
 	for result.Next() {
-		wordTypeId := wordTypeId{}
+		wordTypeId := def.WordTypeId{}
 		if err := result.Scan(&wordTypeId.Word_type_id); err != nil {
 			log.Fatal(err)
 		}
@@ -56,15 +37,6 @@ func GetTypWords(db *sql.DB, values map[string]string) []word {
 	}
 
 	sql = def.GET_TYP_WORDS_SQL
-
-	// for key, value := range values {
-	// 	if key == "quantity" {
-	// 		sql += fmt.Sprintf(" LIMIT '%s'", value)
-	// 	} else {
-	// 		sql += fmt.Sprintf(" AND %s = '%s'", key, value)
-	// 	}
-	// }
-
 	sql = fmt.Sprintf(sql,
 		values["type"],
 		values["parts_of_speech"],
@@ -74,7 +46,7 @@ func GetTypWords(db *sql.DB, values map[string]string) []word {
 	result, err = db.Query(sql)
 
 	for result.Next() {
-		word := word{}
+		word := def.Word{}
 		if err := result.Scan(&word.Word, &word.Parts_of_speech, &word.Description); err != nil {
 			log.Fatal(err)
 		}
@@ -84,16 +56,16 @@ func GetTypWords(db *sql.DB, values map[string]string) []word {
 	return words
 }
 
-func GetTypes(db *sql.DB) []wordType {
+func GetTypes(db *sql.DB) []def.WordType {
 
 	// 複数件取得する場合、構造体を配列にする
-	var wordTypes []wordType
+	var wordTypes []def.WordType
 
 	sql := "SELECT word_type FROM word_types ORDER BY word_type ASC"
 	result, err := db.Query(sql)
 
 	for result.Next() {
-		wordType := wordType{}
+		wordType := def.WordType{}
 		if err := result.Scan(&wordType.Word_type); err != nil {
 			log.Fatal(err)
 		}
@@ -104,21 +76,19 @@ func GetTypes(db *sql.DB) []wordType {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(wordTypes)
-	// fmt.Printf("%T\n", wordTypes)
 	return wordTypes
 }
 
-func GetPartsOfSpeeches(db *sql.DB) []partsOfSpeech {
+func GetPartsOfSpeeches(db *sql.DB) []def.PartsOfSpeech {
 
 	// 複数件取得する場合、構造体を配列にする
-	var partsOfSpeeches []partsOfSpeech
+	var partsOfSpeeches []def.PartsOfSpeech
 
 	sql := "SELECT parts_of_speech FROM parts_of_speeches ORDER BY parts_of_speech ASC"
 	result, err := db.Query(sql)
 
 	for result.Next() {
-		partsOfSpeech := partsOfSpeech{}
+		partsOfSpeech := def.PartsOfSpeech{}
 		if err := result.Scan(&partsOfSpeech.Parts_of_speech); err != nil {
 			log.Fatal(err)
 		}
---
+++ common/structs.go
@@ -0,0 +1,22 @@
+package def
+
+import ()
+
+type (
+	Word struct {
+		Word            string `json:"word"`
+		Parts_of_speech string `json:"parts_of_speech"`
+		Description     string `json:"Description"`
+	}
+	WordType struct {
+		Word_type string `json:"word_type"`
+	}
+
+	PartsOfSpeech struct {
+		Parts_of_speech string `json:"parts_of_speech"`
+	}
+
+	WordTypeId struct {
+		Word_type_id string
+	}
+)
